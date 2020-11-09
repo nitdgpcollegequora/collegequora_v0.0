5f09e706cc0197cbb9763c4b68191d08ba7ebfe2
yaduveera26 <yaduveera26@gmail.com>
2020-11-09 11:06:04 +0530

    contact page added

--- app.js
+++ app.js
@@ -1,75 +1,81 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const nodemailer = require('nodemailer');
 const app = express();
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
 
-mongoose.connect('mongodb://localhost/test',{ useNewUrlParser: true,useUnifiedTopology: true  });
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
 let db = mongoose.connection;
 
-db.once('open',function(){
+db.once('open', function () {
     console.log('connected');
 });
 
-db.on('error',function(err){
+db.on('error', function (err) {
     console.log(err);
 });
 
 let data = require('./models/data');
 
-app.get('/',(req,res)=>{
-    data.find({},function(err,datas){
-        if(err)
-        console.log(err);
-        else{
-            res.render('index',{datas:datas});
+app.get('/', (req, res) => {
+    data.find({}, function (err, datas) {
+        if (err)
+            console.log(err);
+        else {
+            res.render('index', { datas: datas });
         }
     })
 });
 
-app.get('/question',(req,res)=>{
+app.get('/question', (req, res) => {
     res.render('question');
 });
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.post('/question',(req,res)=>{
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+
+
+app.post('/question', (req, res) => {
     let Data = new data();
     Data.name = req.body.subjectcode;
     Data.content = req.body.problem;
-    Data.save(function(err){
-        if(err){
+    Data.save(function (err) {
+        if (err) {
             console.log(err);
         }
-        else{
+        else {
             res.redirect('/');
         }
     })
 });
 
-app.get('/login',(req,res)=>{
+app.get('/login', (req, res) => {
     res.render('login');
 });
 
-app.get('/register',(req,res)=>{
+app.get('/register', (req, res) => {
     res.render('register');
 });
 
-app.get('/forgotpassword',(req,res)=>{
+app.get('/forgotpassword', (req, res) => {
     res.render('forgotpassword');
 });
 
-app.listen('3300',(err)=>{
-    if(err)
-    console.log(err);
+app.listen('3000', (err) => {
+    if (err)
+        console.log(err);
     else
-    console.log('app listening')
-}) ;
+        console.log('app listening');
+});
